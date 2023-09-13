2543339bc128ae9d8b915b2b5f3d766c4a490957
Xellrey <xellray@gmail.com>
2023-09-14 01:34:43 +0300

    test

--- main/models.py
+++ main/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-
+class Books(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    about = models.CharField(max_length=1000)
+    photo = models.ImageField(upload_to="uploads-")
+    count = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    year = models.IntegerField()
+    file = models.FileField(null=True,blank=True)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Books
 # Create your views here.
+
+
+def index(request):
+    book = Books.objects.all()
+    data = {
+        'book': book
+    }
+    return render(request, 'site/main.html', data)
